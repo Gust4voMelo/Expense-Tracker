4eb2fb61784a50cd7681d1f7ff0447616961df02
Gustavo Melo <gustavomelo13205@gmail.com>
2024-06-19 20:28:28 -0300

    Projeto Finalizado

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+   { date: new Date(2024, 5, 17), category: 'food', title: 'Burguer King', value: 35.99 },
+   { date: new Date(2024, 5, 17), category: 'food', title: 'KFC', value: 49.99 },
+   { date: new Date(2024, 5, 17), category: 'rent', title: 'Aluguel Casa', value: 2500 },
+   { date: new Date(2024, 6, 19), category: 'salary', title: 'Salário', value: 5500 }
+];
